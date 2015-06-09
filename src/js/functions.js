d7935648c891518ba213ce92649731b035360dd5
Ricardo <rsoto@globalinteractive.cl>
2015-06-09 17:38:01 -0400

    Cambio de flujo - Ahora avanza solo

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,5 +1,6 @@
 //Aspas de molino
 var rotationSnap = 90;
+var unavez = true;
 Draggable.create(".boxes", {
     type:"rotation", 
     throwProps:true, //enables kinetic-based flicking (continuation of movement, decelerating after releasing the mouse/finger)
@@ -29,18 +30,23 @@ Draggable.create("#molino-inicio", {
   //   $('#box1').addClass('giro');
   // }
   onDragEnd: function(event){
-    var mediaquery = window.matchMedia("(max-width: 768px)");
-    if (mediaquery.matches) {
-       // mobile
-       $('#black, #intro .molino, #intro .right, #textos-inicio, #intro .flecha, nav').removeClass('current');
-    } else {
-      // desktop
-      $('#black, #intro .molino, #intro .right, #textos-inicio, #intro .flecha').removeClass('current');
-      setTimeout(function(){$('nav a').eq(2).click()}, 7000);
-      setTimeout(function(){$('nav a').eq(3).click()}, 17000);
-      setTimeout(function(){$('nav a').eq(4).click();$('nav').removeClass('current');}, 23000);
+    //Se ejecuta callback solo una vez
+    if(unavez){
+      var mediaquery = window.matchMedia("(max-width: 768px)");
+      if (mediaquery.matches) {
+         // mobile
+         $('#black, #intro .molino, #intro .right, #textos-inicio, #intro .flecha, nav').removeClass('current');
+      } else {
+        // desktop
+        $('#black, #intro .molino, #intro .right, #textos-inicio, #intro .flecha').removeClass('current');
+        setTimeout(function(){$('nav a').eq(2).click()}, 7000);
+        setTimeout(function(){$('nav a').eq(3).click()}, 17000);
+        setTimeout(function(){$('nav a').eq(4).click();$('nav').removeClass('current');}, 23000);
+      }
     }
+    unavez = false;
   },
+  //Al click sobre astas desaparecer flecha indicadora
   onPress: function(event){
     $('#textos-inicio img').css('opacity', '0');
   },
